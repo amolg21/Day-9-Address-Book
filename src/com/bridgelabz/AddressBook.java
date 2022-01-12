8baaeb94e8130bf2e58bd1affaefec5d5a1cae40
Amol Ghotale <amol9.ag@gmail.com>
2022-01-12 18:27:19 +0530

    ADDED: New class, User input

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,8 +1,35 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBook {
+    private Scanner scanner =new Scanner(System.in);
+
+    public Contacts addContacts () {
+
+        Contacts con = new Contacts();
+
+        System.out.println("Enter FirstName ");
+        String firstName=scanner.nextLine();
+        System.out.println("Enter LastName ");
+        String lastName=scanner.nextLine();
+        System.out.println("Enter Address ");
+        String address=scanner.nextLine();
+        System.out.println("Enter City ");
+        String city=scanner.nextLine();
+        System.out.println("Enter State ");
+        String state=scanner.nextLine();
+        System.out.println("Enter Zip Code ");
+        String zip=scanner.nextLine();
+        System.out.println("Enter Mobile No ");
+        String mobileNo=scanner.nextLine();
+        System.out.println("Enter email ");
+        String email=scanner.nextLine();
+
+        return con;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        
     }
 }
